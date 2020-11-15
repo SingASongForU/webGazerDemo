6ca254e5d58a83b35c41433e2071e778dbf2de73
xiefei <applemechanic@163.com>
2020-11-15 23:54:52 +0800

    fix bug

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,10 +1,10 @@
 webgazer.setGazeListener(function(data, elapsedTime) {
     if (data == null) {
+        console.log("Cannot get any data from webGazer.")
         return;
     }
     var xprediction = data.x; //these x coordinates are relative to the viewport
     var yprediction = data.y; //these y coordinates are relative to the viewport
+    console.log(data);
     console.log(elapsedTime); //elapsed time is based on time since begin was called
-    console.log(xprediction)
-    console.log(yprediction);
 }).begin();
