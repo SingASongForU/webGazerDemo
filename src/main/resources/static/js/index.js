82a83b5146a7fe567d6331be483d07455d343fe2
unknown <415801746@qq.com>
2020-11-17 15:09:57 +0800

    page optimize

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,10 +1,14 @@
+window.saveDataAcrossSessions(false);
 webgazer.setGazeListener(function(data, elapsedTime) {
     if (data == null) {
-        console.log("Cannot get any data from webGazer.")
+        console.log("训练中。。。")
         return;
     }
     var xprediction = data.x; //these x coordinates are relative to the viewport
     var yprediction = data.y; //these y coordinates are relative to the viewport
-    console.log(data);
-    console.log(elapsedTime); //elapsed time is based on time since begin was called
+    if(data!=null){
+    	console.log(data);
+        console.log(elapsedTime); //elapsed time is based on time since begin was called
+    }
 }).begin();
+
