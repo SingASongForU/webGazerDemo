aa84e3697a633bd3cfd46684c117e4eeea99b6f8
unknown <415801746@qq.com>
2020-11-17 10:53:36 +0800

    solve the problem that no eyes tracking and no data get commit by xiefei

--- src/main/resources/static/js/hello.js
+++ src/main/resources/static/js/hello.js
@@ -1,1 +1,10 @@
-alert("hello world");
+webgazer.setGazeListener(function(data, elapsedTime) {
+    if (data == null) {
+        console.log("Cannot get any data from webGazer.")
+        return;
+    }
+    var xprediction = data.x; //these x coordinates are relative to the viewport
+    var yprediction = data.y; //these y coordinates are relative to the viewport
+    console.log(data);
+    console.log(elapsedTime); //elapsed time is based on time since begin was called
+}).begin();
